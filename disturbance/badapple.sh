6141f85c25994c2c5c352bdfd7dbb6e826e8b05f
Aknee <annyhua04@gmail.com>
2026-04-07 21:09:25 -0400

    meow

--- disturbance/badapple.sh
+++ disturbance/badapple.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 /opt/CharAnimePlayer/CharAnimePlayer.py /opt/CharAnimePlayer/bad-apple.mp4 -fps 50 -width 100 -height 100 --raw
+python3 /opt/CharAnimePlayer/CharAnimePlayer.py /opt/CharAnimePlayer/bad-apple.mp4 -fps 50 -width 10 -height 10 --raw
